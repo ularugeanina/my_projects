cb612e098c3f455f0485f0802276d4300c4cac2c
geanina.ularu <geanina.ularu@gooddata.com>
2013-12-27 19:53:16 -0800

    initial commit

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :email, :address, :active, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '496c34f41a53609c5b09a9888ab0adf93dc5ed0d27cc8ab907d2469ffd91d336049cd217e58f2db8fc1d8b4dfbfa5693838c6c5a1251def55d357a913fc22f75'
